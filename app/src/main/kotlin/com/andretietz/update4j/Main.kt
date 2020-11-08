7cb820c1b62e3c4a6f9994e143e28e69c934faa3
André Tietz <andre.tietz@gmail.com>
2020-11-08 17:29:16 +0100

    preparing PR

--- app/src/main/kotlin/com/andretietz/update4j/Main.kt
+++ app/src/main/kotlin/com/andretietz/update4j/Main.kt
@@ -1,11 +1,10 @@
 package com.andretietz.update4j
 
-
 class Main {
   companion object {
     @JvmStatic
     fun main(args: Array<String>) {
-      println("Hello World!")
+      println(LocalLibrary.returnSomeString())
     }
   }
 }
--- locallib/src/main/kotlin/com/andretietz/update4j/BullShit.kt
+++
@@ -1,5 +0,0 @@
-package com.andretietz.update4j
-
-object BullShit {
-  fun someExternalLib() = "foo"
-}
